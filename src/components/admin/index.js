8706c2a24e8f86566a211733500e31dc64936a99
Emmanuel <iemm96@gmail.com>
2019-12-03 22:22:42 -0600

    ·Se cambió el Dashboard de admin por Tabs ·Archivo de rutas actualizado

---
+++ src/components/admin/index.js
@@ -0,0 +1,151 @@
+import React, {useState} from 'react';
+import {NavItem, NavLink, TabContent, TabPane, UncontrolledDropdown} from "reactstrap";
+
+import DropdownToggle from "reactstrap/es/DropdownToggle";
+import DropdownMenu from "reactstrap/es/DropdownMenu";
+import DropdownItem from "reactstrap/es/DropdownItem";
+
+export default class AdminDashboard extends React.Component{
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            setActiveTab: 1,
+            activeTab: 1,
+            dropDownValue: 'Select action',
+            dropdownOpen: false
+        }
+    }
+
+    componentDidMount() {
+
+        let self = this;
+        var elements = document.querySelectorAll('[data-toggle="sticky-onscroll"]');
+        // Find all data-toggle="sticky-onscroll" elements
+
+        const url = this.props.match.path;
+
+
+        [].forEach.call(elements, function(element) {
+
+            let div = document.createElement('div');
+            div.classList.add('sticky-wrapper');
+
+            var sticky = element;
+            var stickyWrapper = div;
+            sticky.before(stickyWrapper);
+            sticky.classList.add('sticky');
+
+            // Scroll & resize events
+            window.addEventListener('scroll', function () {
+                self.stickyToggle(sticky, stickyWrapper, window);
+            });
+
+            // On page load
+            self.stickyToggle(sticky, stickyWrapper, window);
+        });
+
+    }
+
+    changeValue(e)
+    {
+        this.setState({dropDownValue: e.currentTarget.textContent})
+    }
+
+    toggle(tab) {
+        console.log(tab);
+        if(this.state.activeTab !== tab) {
+            this.setState({activeTab:tab})
+        }
+    }
+
+    render() {
+
+        return (
+
+            <div className="mt-3">
+                <header className="">
+                    <nav className="header-dashboard navbar navbar-expand-lg navbar-light top-navbar  animate fadeInDown one" data-toggle="sticky-onscroll">
+                        <div className="container">
+
+                            <NavLink className="navbar-brand" to="#">CondoSpace</NavLink>
+                            <button className="navbar-toggler" type="button" data-toggle="collapse"
+                                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                                    aria-expanded="false" aria-label="Toggle navigation">
+                                <span className="navbar-toggler-icon">i</span>
+                            </button>
+                            <div className="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
+                                <ul className="navbar-nav">
+                                    <li className="nav-item">
+                                        <NavLink className="nav-link" href="#" onClick={() => {this.toggle(1)}} className={this.state.activeTab === 1 ? 'active' : ''}>Anuncios</NavLink>
+                                    </li>
+                                    <li className="nav-item">
+                                        <NavLink className="nav-link" href="#" onClick={() => {this.toggle(2)}} className={this.state.activeTab === 2 ? 'active' : ''}>Eventos</NavLink>
+                                    </li>
+                                    <li className="nav-item">
+                                        <NavLink className="nav-link" href="#" onClick={() => {this.toggle(3)}} className={this.state.activeTab === 3 ? 'active' : ''}>Finanzas</NavLink>
+                                    </li>
+                                    <li className="nav-item">
+                                        <NavLink className="nav-link" href="#"  onClick={() => {this.toggle(4)}} className={this.state.activeTab === 4 ? 'active' : ''}>Áreas Comunes</NavLink>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div className="justify-content-end" id="navbarSupportedContent">
+                                <ul className="navbar-nav">
+                                    <img src={require('../../assets/images.png')} width={35} height={35} className="rounded-circle"/>
+                                    <UncontrolledDropdown>
+                                        <DropdownToggle caret>
+                                            Nombre del usuario
+                                        </DropdownToggle>
+                                        <DropdownMenu>
+                                            <DropdownItem>Mis datos de perfil</DropdownItem>
+                                            <DropdownItem divider />
+                                            <DropdownItem>Cerrar Sesión</DropdownItem>
+                                        </DropdownMenu>
+                                    </UncontrolledDropdown>
+                                </ul>
+                            </div>
+                        </div>
+                    </nav>
+                </header>
+                <div className="dashboard-content animate fadeInUp one">
+                    <TabContent activeTab={this.state.activeTab} className="text-center">
+                        <TabPane className={this.state.activeTab === 1 ? 'active' : ''} tabId="1">
+                            <div className="p-5">
+                            </div>
+                        </TabPane>
+                        <TabPane className={this.state.activeTab === 2 ? 'active' : ''} tabId="2">
+                            <p>2</p>
+                        </TabPane>
+                        <TabPane className={this.state.activeTab === 3 ? 'active' : ''} tabId="3">
+                            <p>3</p>
+                        </TabPane>
+                    </TabContent>
+                </div>
+            </div>
+        );
+
+    }
+
+    stickyToggle(sticky, stickyWrapper, scrollElement) {
+
+        var stickyHeight = sticky.offsetHeight;
+        var stickyTop = stickyWrapper.offsetTop;
+
+        if (scrollElement.pageYOffset >= stickyTop) {
+            sticky.classList.add("is-sticky");
+
+            stickyWrapper.style.height = stickyHeight + 'px';
+        } else {
+            sticky.classList.remove("is-sticky");
+            stickyWrapper.style.height = 'auto';
+        }
+    };
+
+    clearInput(e)
+    {
+        e.target.classList.remove('bounce');
+    }
+
+};
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Login from "../Login";
+import AdminDashboard from "../components/admin";
 import Anuncios from "../admin/Anuncios";
 import Finanzas from "../admin/Finanzas";
 import Eventos from "../admin/Eventos";
@@ -9,7 +10,7 @@ import AreasComunes from "../admin/AreasComunes";
 const AppRoutes = () =>
     <BrowserRouter>
         <Switch>
-            <Route path="/admin/anuncios" component={Anuncios}/>
+            <Route path="/admin/index" component={AdminDashboard}/>
             <Route path="/admin/finanzas" component={Finanzas}/>
             <Route path="/admin/eventos" component={Eventos}/>
             <Route path="/admin/areasComunes" component={AreasComunes}/>
