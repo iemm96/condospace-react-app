495ed41e22340838e9695a940563c65125ad50ec
JairoGlez9612 <55265932+JairoGlez9612@users.noreply.github.com>
2020-03-20 18:43:34 -0600

    Revert "Ui update"

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,17 +2,17 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Login from "../Login";
 import AdminDashboard from "../components/admin";
+import Anuncios from "../admin/Anuncios";
+import Finanzas from "../admin/Finanzas";
+import Eventos from "../admin/Eventos";
 import AreasComunes from "../admin/AreasComunes";
-import Anuncios from "../components/admin/sections/Anuncios";
-import Eventos from "../components/admin/sections/Eventos";
-import Finanzas from "../components/admin/sections/Finanzas";
+
 const AppRoutes = () =>
     <BrowserRouter>
         <Switch>
             <Route path="/admin/index" component={AdminDashboard}/>
-            <Route path="/admin/anuncios" component={Anuncios}/>
-            <Route path="/admin/eventos" component={Eventos}/>
             <Route path="/admin/finanzas" component={Finanzas}/>
+            <Route path="/admin/eventos" component={Eventos}/>
             <Route path="/admin/areasComunes" component={AreasComunes}/>
             <Route path="/" component={Login}/>
         </Switch>
