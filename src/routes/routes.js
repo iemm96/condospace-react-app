bb72194eb0db240054e49eff5108cdc4382f0cf9
Jairo Gonzalez <jairoj_glez@hotmail.com>
2020-03-21 23:48:54 -0600

    Sections
    Se crearon las sections de las tablas que faltaban

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,7 +4,6 @@ import Login from "../Login";
 import AdminDashboard from "../components/admin";
 import AreasComunes from "../admin/AreasComunes";
 
-i
 import Anuncios from "../components/admin/sections/Anuncios";
 import Eventos from "../components/admin/sections/Eventos";
 import Finanzas from "../components/admin/sections/Finanzas";
