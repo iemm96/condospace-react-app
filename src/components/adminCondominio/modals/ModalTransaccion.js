1a2e4e82dcbfcdea2af9f0b11fd5ad727a6d919c
Emmanuel <emmanuel@nucleodev.com>
2020-06-09 08:40:31 -0500

    Se agregaron utilidades de fecha
    ·Librerías actualizadas

---
+++ src/components/adminCondominio/modals/ModalTransaccion.js
@@ -0,0 +1,452 @@
+import React, { useState, useEffect } from 'react';
+import { Modal, ModalHeader, ModalBody, ModalFooter, Button, Form, FormGroup, Label, Input, FormText, Col, Row } from 'reactstrap';
+import Select from "react-select";
+
+import {updateRecord} from "../../../actions/updateRecord";
+import {storeRecord} from "../../../actions/storeRecord";
+import {useForm} from "react-hook-form";
+import DatePicker, { registerLocale } from "react-datepicker/es";
+import "react-datepicker/dist/react-datepicker.css";
+import es from "date-fns/locale/es";
+import {format} from "date-fns";
+import {fetchRecordsByParam} from "../../../actions/fetchRecordsByParam";
+import {useUsuario} from "../../../context/usuario-context";
+import {store} from "react-notifications-component";
+import {fetchRecord} from "../../../actions/fetchRecord";
+registerLocale("es", es);
+
+
+const ModalTransaccion = (props) => {
+    const { register, handleSubmit } = useForm();
+    const { idCondominio } = useUsuario();
+    const [recordState, setRecordState] = useState(props);
+    const [startDate, setStartDate] = useState(new Date());
+    const [tipoTransaccion,setTipoTransaccion] = useState(null);
+    const [tipoUnidad,setTipoUnidad] = useState(null);
+    const [tipoCuenta,setTipoCuenta] = useState(null);
+    const [tipoFormaPago,setTipoFormaPago] = useState([]);
+    const [record,setRecord] = useState(null);
+    const [unidades,setUnidades] = useState([]);
+    const [cuentas,setCuentas] = useState([]);
+    const [cuentasTotal,setCuentasTotal] = useState([]);
+    const [total,setTotal] = useState(null);
+    const [disabledButton,setDisabledButton] = useState(true);
+
+    useEffect(() => {
+        setRecordState(props);
+
+        //Obtiene los datos del registro
+        async function getRecord() {
+            try {
+
+                const resultadoRecord = await fetchRecord(props.idRecord,'transacciones');
+                setTipoUnidad(resultadoRecord.idUnidad);
+                setTipoCuenta(resultadoRecord.idCuenta);
+                setTipoFormaPago(resultadoRecord.formaPago);
+                setTipoTransaccion(resultadoRecord.esIngreso);
+                setStartDate(new Date(resultadoRecord.fechaCobro));
+
+                setRecord(resultadoRecord);
+            }catch (e) {
+                console.log(e);
+            }
+        }
+
+        if(props.idRecord) {
+            getRecord();
+        }
+    }, [props]);
+
+    useEffect(() => {
+        async function getUnidades() {
+
+            try {
+                const resultadoUnidades = await fetchRecordsByParam('getUnidadesByCondominio',idCondominio);
+
+                let opcionesUnidades = [];
+
+                if(resultadoUnidades) {
+                    resultadoUnidades.map((val) => {
+                        opcionesUnidades.push({value:val.idUnidad,label:val.nombre,name:'idUnidad'});
+                    });
+
+                    setUnidades(opcionesUnidades);
+                }
+            }catch (e) {
+                console.log(e);
+            }
+        }
+        
+        async function getCuentas() {
+            try {
+                const resultadoCuentas = await fetchRecordsByParam('getCuentasByCondominio',idCondominio);
+
+                let opcionesCuentas = [];
+
+                if(resultadoCuentas) {
+                    resultadoCuentas.map((val) => {
+                        opcionesCuentas.push({value:val.idCuenta,label:`${val.nombre} ($${new Intl.NumberFormat().format(val.total)})`,name:'idCuenta'});
+                        cuentasTotal.push({idCuenta:val.idCuenta,total:val.total});
+                    });
+
+                    setCuentas(opcionesCuentas);
+                }
+            }catch (e) {
+                console.log(e);
+            }
+        }
+
+
+
+        getUnidades();
+        getCuentas();
+
+
+    },[record]);
+
+    useEffect(() => {
+        return () => {
+            console.log("cleaned up");
+        };
+    }, []);
+
+    const handleInputChange = event => {
+
+        console.log(event);
+        let target;
+
+        if(target = event.target) {
+            const value = target.value;
+            const name = target.name;
+            this.setState({
+                [name]:value
+            });
+        }else{
+            const name = event.name;
+            const value = event.value;
+            this.setState({
+                [name]:value
+            })
+        }
+    };
+
+    const handleChangeCuenta = (idCuenta) => {
+        setTipoCuenta(idCuenta);
+        handleChangeTotal();
+    };
+
+    const handleChangeTotal = () => {
+
+        if(!tipoCuenta) {
+            return;
+        }
+
+        const idCuenta = tipoCuenta;
+
+        //Se busca el total de la cuenta en el arreglo cuentasTotal, si se encuentra se regresa
+        const cuenta = cuentasTotal.find(function(value, index) {
+            if(value.idCuenta === idCuenta) {
+                return value.total;
+            }
+        });
+
+        let inputMonto = document.getElementById('inputMonto');
+        let radioIngreso = document.getElementById('radioIngreso');
+        let radioEgreso = document.getElementById('radioEgreso');
+
+        if(inputMonto.value > 0){
+
+            if(radioIngreso.checked) {
+                setTotal(parseInt(cuenta.total) + parseInt(inputMonto.value));
+            }else if(radioEgreso.checked) {
+
+                const result = parseInt(cuenta.total) - parseInt(inputMonto.value);
+
+                if(result > 0) {
+                    setTotal(result);
+                }else {
+                    return;
+                }
+            }
+
+            setDisabledButton(false);
+        }
+
+    };
+
+
+    const onSubmit = async (data) => {
+        console.log(data);
+
+        console.log(startDate);
+
+        let date = format(startDate,'yyyy-i-dd');
+        console.log(date);
+        console.log('tipoUnidad ',tipoUnidad);
+        console.log('tipoFormaPago ',tipoFormaPago);
+        console.log('tipoCuenta ',tipoCuenta);
+
+        data.fechaCobro = format(startDate,'yyyy-i-dd');
+        data.fechaHora_transaccion = format(new Date(),'yyyy-i-dd H:i:s');
+        data.idCuenta = tipoCuenta;
+        data.formaPago = tipoFormaPago;
+        data.idUnidad = tipoUnidad;
+
+        console.log(data);
+        if(record) {
+
+            try {
+                const response = await updateRecord(data,'transacciones',record.idTransaccion);
+                if(response) {
+                    store.addNotification({
+                        title: "Correcto",
+                        message: "Se ha actualizado la transacción",
+                        type: "success",
+                        insert: "top",
+                        container: "top-right",
+                        animationIn: ["animated", "fadeIn"],
+                        animationOut: ["animated", "fadeOut"],
+                        dismiss: {
+                            duration: 5000,
+                            onScreen: true
+                        }
+                    });
+                    props.toggleModal();
+                    props.updateRecords();
+                }
+            }catch (e) {
+                console.log(e);
+                store.addNotification({
+                    title: "Ocurrió un error al actualizar la transacción",
+                    message: "Revisa tu conexión a internet",
+                    type: "danger",
+                    insert: "top",
+                    container: "top-right",
+                    animationIn: ["animated", "fadeIn"],
+                    animationOut: ["animated", "fadeOut"],
+                    dismiss: {
+                        duration: 5000,
+                        onScreen: true
+                    }
+                });
+            }
+
+        }else{
+            try {
+
+
+                const response = await storeRecord(data,'addTransaccion');
+                if(response) {
+                    store.addNotification({
+                        title: "Correcto",
+                        message: "Se ha creado una nueva transacción",
+                        type: "success",
+                        insert: "top",
+                        container: "top-right",
+                        animationIn: ["animated", "fadeIn"],
+                        animationOut: ["animated", "fadeOut"],
+                        dismiss: {
+                            duration: 5000,
+                            onScreen: true
+                        }
+                    });
+                    props.toggleModal();
+                    props.updateRecords();
+                }
+
+
+            }catch (e) {
+                console.log(e);
+                store.addNotification({
+                    title: "Ocurrió un error al agregar la transacción",
+                    message: "Revisa tu conexión a internet",
+                    type: "danger",
+                    insert: "top",
+                    container: "top-right",
+                    animationIn: ["animated", "fadeIn"],
+                    animationOut: ["animated", "fadeOut"],
+                    dismiss: {
+                        duration: 5000,
+                        onScreen: true
+                    }
+                });
+            }
+
+
+        }
+
+    };
+
+    const customStyles = {
+
+        control: () => ({
+            borderRadius: 10,
+            border: '1px solid #979797 !important',
+            position: 'relative',
+            justifyContent: 'space-between',
+            display: '-webkit-flex'
+        }),
+        dropdownIndicator: () => ({
+            color: 'black',
+            marginRight: 10
+        }),
+        indicatorSeparator: () => ({
+            border: 'none',
+        })
+
+    }
+
+    const formasPago = [
+        {value:1,label:'Efectivo',name:'formaPago'},
+        {value:2,label:'Cheque',name:'formaPago'},
+    ];
+
+    return(<Modal size="lg" isOpen={props.recordModal} toggle={() => props.toggleModal()}>
+        <ModalHeader toggle={() => props.toggleModal()}>{props.idRecord ? 'Actualizar' : 'Crear'} Transacción</ModalHeader>
+        <ModalBody>
+            <Form id="form" onSubmit={handleSubmit(onSubmit)}>
+                <Row>
+                    <Col sm={8}>
+                        <FormGroup>
+                            <Label>* Concepto</Label>
+                            <input className="form-control" type="text" name="concepto" id="" placeholder=""
+                                   defaultValue={record ? record.concepto : undefined}
+                                   ref={register({required:true})}/>
+                        </FormGroup>
+                    </Col>
+                    <Col sm={4}>
+                        <FormGroup>
+                            <Label>* Monto</Label>
+                            <input className="form-control" type="number" name="monto" id="inputMonto" placeholder=""
+                                   defaultValue={record ? record.monto : undefined}
+                                   onChange={handleChangeTotal}
+                                   ref={register}/>
+                        </FormGroup>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col sm={8}>
+                        <FormGroup>
+                            <Label>Descripción</Label>
+                            <textarea className="form-control" name="descripcion" id="" placeholder=""
+                                      defaultValue={record ? record.descripcion : undefined}
+                                   ref={register}/>
+                        </FormGroup>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col sm={4}>
+                        <FormGroup>
+                            <label>* ¿Es ingreso o egreso?</label>
+                            <div className="custom-control custom-radio custom-control-inline">
+                                <input type="radio"
+                                       value="1"
+                                       className="custom-control-input"
+                                       id="radioIngreso"
+                                       name="esIngreso"
+                                       ref={register}
+                                       onChange={handleChangeTotal}
+                                       />
+                                    <label className="custom-control-label" htmlFor="radioIngreso">Ingreso</label>
+                            </div>
+                            <div className="custom-control custom-radio custom-control-inline">
+                                <input type="radio"
+                                       value="0"
+                                       className="custom-control-input"
+                                       id="radioEgreso"
+                                       name="esIngreso"
+                                       ref={register}
+                                       onChange={handleChangeTotal}
+                                       />
+                                    <label className="custom-control-label" htmlFor="radioEgreso">Egreso</label>
+                            </div>
+                        </FormGroup>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col sm={4}>
+                        <FormGroup>
+                            <Label>* Unidad</Label>
+                            <Select styles={customStyles}
+                                    options={unidades}
+                                    placeholder="Selecciona una opción..."
+                                    ref={register}
+                                    value={unidades.find(op => {
+                                        return op.value === tipoUnidad
+                                    })}
+                                    onChange={(event) => {setTipoUnidad(event.value)}}
+                            />
+                        </FormGroup>
+                    </Col>
+
+                </Row>
+                <Row>
+                    <Col sm={4}>
+                        <FormGroup>
+                            <Label>* Forma de pago</Label>
+                            <Select styles={customStyles}
+                                    options={formasPago}
+                                    placeholder="Selecciona una opción..."
+                                    value={formasPago.find(op => {
+                                        return op.value === tipoFormaPago
+                                    })}
+                                    onChange={(event) => {setTipoFormaPago(event.value)}}
+                            />
+                        </FormGroup>
+                    </Col>
+                    <Col sm={4}>
+                        <FormGroup>
+                            <Label>Referencia</Label>
+                            <input className="form-control" type="text" name="referencia" id="" placeholder=""
+                                   defaultValue={record ? record.referencia : undefined}
+                                   ref={register}/>
+                        </FormGroup>
+                    </Col>
+
+                </Row>
+                <Row>
+                    <Col sm={4}>
+                        <FormGroup>
+                            <Label>* Cuenta</Label>
+                            <Select styles={customStyles}
+                                    options={cuentas}
+                                    value={cuentas.find(op => {
+                                        return op.value === tipoCuenta
+                                    })}
+                                    placeholder="Selecciona una opción..."
+                                    onChange={(event) => {handleChangeCuenta(event.value)}}
+                                    />
+                        </FormGroup>
+                        <Row>
+                            <Col>
+                                {total ? 'El total de la cuenta será de $' + new Intl.NumberFormat().format(total) : ''}
+                            </Col>
+                        </Row>
+                    </Col>
+                    <Col sm={4}>
+                        <FormGroup>
+                            <Label>* Se cobró el día</Label>
+                            <DatePicker
+                                locale="es"
+                                className="form-control"
+                                maxDate={new Date()}
+                                name="fechaCobro"
+                                dateFormat="dd/MMMM/yyyy"
+                                selected={startDate}
+                                onChange={date => setStartDate(date)} />
+                        </FormGroup>
+                    </Col>
+                </Row>
+            </Form>
+        </ModalBody>
+        <p className="center">Los campos marcados con * son obligatorios</p>
+        <ModalFooter className="d-flex justify-content-around">
+            <Button className="neutralButton" onClick={() => props.toggleModal()}>Cancelar</Button>
+            <Button className="confirmButton" type="submit" disabled={disabledButton} form="form" color="primary">{props.idRecord ? 'Actualizar ' : 'Crear '} Transacción</Button>
+        </ModalFooter>
+    </Modal>);
+};
+
+export default ModalTransaccion;
+
+
---
+++ src/components/adminCondominio/tables/TransaccionesTable.js
@@ -0,0 +1,236 @@
+import React,{useState,useEffect} from "react";
+import ToolkitProvider from "react-bootstrap-table2-toolkit";
+import BootstrapTable from "react-bootstrap-table-next";
+import paginationFactory, {PaginationListStandalone, PaginationProvider} from "react-bootstrap-table2-paginator";
+import {Button, Col} from "reactstrap";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faEdit, faEye, faTrash} from "@fortawesome/free-solid-svg-icons";
+import {fetchRecords} from "../../../actions/fetchRecords";
+import Skeleton from 'react-loading-skeleton';
+import {Buscador} from './../common/buscador';
+import {options} from "../../../constants/tables_options";
+import {DeleteRecordModal} from "../modals/DeleteRecordModal";
+//Change
+import ModalRecord from "../modals/ModalTransaccion";
+import {useUsuario} from "../../../context/usuario-context";
+
+//Change
+const RESOURCE = 'transacciones'; //API
+const NEW_BUTTON_TEXT = 'Nueva Transacción';
+const PLACEHOLDER_SEARCH_TEXT = `Buscar ${RESOURCE}...`;
+
+
+const TransaccionesTable  = (props) => {
+    const {idCondominio} = useUsuario();
+    const [records,setRecords] = useState(null);
+    const [selectedRecordId,setSelectedRecordId] = useState(null);
+    const [selectedRecordTitle,setSelectedRecordTitle] = useState(null);
+    const [modalControl,setModalControl] = useState(false);
+    const [modalDeleteControl,setModalDeleteControl] = useState(false);
+    const [expanded,setExpanded] = useState([]);
+
+    useEffect(() => {
+
+        async function getRecords() {
+            try {
+                const result = await fetchRecords(`transacciones/getRecords/${idCondominio}`);
+                setRecords(result);
+            }catch (e) {
+                console.log(e);
+            }
+        }
+        getRecords();
+    },[]);
+
+    const prepareEditModal = (idRecord) => {
+        setSelectedRecordId(idRecord);
+        toggleModal();
+    };
+
+    const prepareNewModal = () => {
+        setSelectedRecordId(null);
+        toggleModal();
+    };
+
+    const prepareDeleteModal = (id,title) => {
+        setSelectedRecordId(id);
+        setSelectedRecordTitle(title);
+        toggleDeleteModal();
+    };
+
+    const actionsFormatter = (cell, row) => (<div className="d-flex justify-content-center">
+            <Button type="Button" onClick={() => handleExpandButtonClick(row)} className="btn mr-2 btnAction"><FontAwesomeIcon icon={faEye}/></Button>
+            <Button type="Button" onClick={() => prepareEditModal(row.idTransaccion)} className="btn mr-2 btnAction"><FontAwesomeIcon icon={faEdit}/></Button>
+            <Button type="Button" onClick={() => prepareDeleteModal(row.idTransaccion, row.concepto)} className="btn btnAction"><FontAwesomeIcon icon={faTrash} /></Button>
+        </div>
+    );
+
+    const columns = [{
+        dataField: 'folio',
+        text: 'Folio',
+        sort: true,
+        headerStyle: { backgroundColor: '#011627', color: 'white',borderRadius:'18px 0 0 0'}
+    },{
+        dataField: 'fechaCobro',
+        text: 'Fecha',
+        sort: true,
+        headerStyle: { backgroundColor: '#011627', color: 'white'}
+    },{
+        dataField: 'concepto',
+        text: 'Concepto',
+        sort: true,
+        headerStyle: { backgroundColor: '#011627', color: 'white'}
+    },{
+        dataField: 'unidad',
+        text: 'Unidad',
+        sort: true,
+        headerStyle: { backgroundColor: '#011627', color: 'white'}
+    },{
+        dataField: 'monto',
+        text: 'Monto',
+        sort: true,
+        headerStyle: { backgroundColor: '#011627', color: 'white'}
+    },{
+        dataField: 'actions',
+        text: 'Acciones',
+        isDummyField: true,
+        csvExport: false,
+        formatter: actionsFormatter,
+        headerStyle: { backgroundColor: '#011627', color: 'white',borderRadius:'0 18px 0 0', textAlign:'center'}
+    }];
+
+    const toggleModal = () => {
+        setModalControl(!modalControl);
+    };
+
+    const toggleDeleteModal = () => {
+        setModalDeleteControl(!modalDeleteControl);
+    };
+
+    const updateRecords = async () => {
+        console.log('updating');
+        const result = await fetchRecords(`transacciones/getRecords/${idCondominio}`);
+        if(result) {
+            setRecords(result);
+        }
+    };
+
+    const rowStyle = (row, rowIndex) => {
+
+        if(row.esIngreso === 1) {
+            return { backgroundColor: '#A2F8B1' };
+        }else{
+            return { backgroundColor: '#FFAFCD' };
+        }
+        
+    };
+    
+    const handleExpandButtonClick = (row) => {
+    console.log(expanded);
+        if (!expanded.includes(row.idTransaccion)) {
+
+            setExpanded([...expanded,row.idTransaccion]);
+        } else {
+            setExpanded(expanded.filter(x => x !== row.idTransaccion));
+        }
+    };
+
+    const handleOnExpand = (row, isExpand, rowIndex, e) => {
+        if (isExpand) {
+            setExpanded([...expanded, row.idTransaccion]);
+        } else {
+            setExpanded(expanded.filter(x => x !== row.idTransaccion));
+        }
+    };
+
+
+
+    const expandRow =  {
+        renderer: row => (
+            <div>
+                <p><b>Descripción:</b> {row.descripcion}</p>
+                <p><b>Forma de pago:</b> {row.formaPago === 1 ? 'Efectivo' : 'Cheque'}</p>
+                {row.referencia ? <p><b>Referencia:</b> {row.referencia}</p> : ''}
+                <p><b>Cuenta:</b> {row.nombre}</p>
+            </div>
+        ),
+        expanded: expanded,
+        onExpand: handleOnExpand,
+        onlyOneExpanding: true,
+        expandByColumnOnly: true
+    };
+
+    const contentTable = ({ paginationProps, paginationTableProps }) => (
+        <div>
+            <ModalRecord
+                idRecord={selectedRecordId}
+                toggleModal={toggleModal}
+                recordModal={modalControl}
+                resource={RESOURCE}
+                updateRecords={updateRecords}
+            />
+            <DeleteRecordModal
+                toggleDeleteModal={toggleDeleteModal}
+                title={selectedRecordTitle}
+                idRecord={selectedRecordId}
+                deleteModal={modalDeleteControl}
+                resource={RESOURCE}
+            />
+            <ToolkitProvider
+                keyField="idTransaccion"
+                columns={ columns }
+                data={ records }
+                search>
+                {
+                    toolkitprops => (
+                        <div>
+                            <Buscador prepareNewModal={prepareNewModal}
+                                      buttonText={NEW_BUTTON_TEXT}
+                                      placeholderText={PLACEHOLDER_SEARCH_TEXT}
+                                      { ...toolkitprops.searchProps }
+                            />
+                            <div className="mt-5">
+                                <BootstrapTable
+                                    isKey={true}
+                                    bordered={false}
+                                    hover
+                                    rowStyle={ rowStyle }
+                                    expandRow={ expandRow }
+                                    { ...toolkitprops.baseProps }
+                                    { ...paginationTableProps }
+                                />
+                            </div>
+
+                        </div>
+                    )
+                }
+            </ToolkitProvider>
+            <PaginationListStandalone { ...paginationProps } />
+        </div>
+    );
+
+    if(records) {
+
+        return(
+            <div>
+                <Col className="col-3">
+                </Col>
+                <PaginationProvider
+                    pagination={paginationFactory(options(records))}>
+
+                    {contentTable}
+
+                </PaginationProvider>
+            </div>
+        );
+    }else{
+        return(<div style={{ fontSize: 20, lineHeight: 2 }}>
+            <h1>{<Skeleton />}</h1>
+            {<Skeleton count={5} />}
+        </div>);
+    }
+};
+
+export default TransaccionesTable;
+
+
