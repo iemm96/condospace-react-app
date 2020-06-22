2e341eada2c47b2f6bb7c906cd2b872ad9dcdefc
Emmanuel <emmanuel@nucleodev.com>
2020-06-21 20:24:18 -0500

    Se actualiza el diseño del modal de eliminar

--- src/components/admin/modals/DeleteRecordModal.js
+++ src/components/admin/modals/DeleteRecordModal.js
@@ -10,9 +10,9 @@ export const  DeleteRecordModal = (props) => {
         <ModalBody>
             <p>¿Seguro que desea eliminar el registro de <b>'{props.title}'</b>? Esta acción no se puede deshacer.</p>
         </ModalBody>
-        <ModalFooter>
-            <Button color="secondary" onClick={() => props.toggleDeleteModal()}>Cancelar</Button>
-            <Button color="primary" onClick={() => deleteRecord(props.idRecord, props.resource)}>Eliminar
+        <ModalFooter  className="d-flex justify-content-around">
+            <Button className="neutralButton" onClick={() => props.toggleDeleteModal()}>Cancelar</Button>
+            <Button className="confirmButton" onClick={() => deleteRecord(props.idRecord, props.resource)}>Eliminar
                 Registro</Button>{' '}
         </ModalFooter>
     </Modal>);
