3d119ed8600c15fbfa3a3f48d007ac254f0a37c8
Emmanuel <emmanuel@nucleodev.com>
2020-06-28 17:57:28 -0500

    Se agrega el header idCondominio a fetchRecordsByParam

--- src/actions/fetchRecordsByParam.js
+++ src/actions/fetchRecordsByParam.js
@@ -3,19 +3,31 @@ import CookieService from "../services/CookieService";
 import axios from "axios";
 const api_url = url_base;
 
-export const fetchRecordsByParam = async (resource,param) => {
+export const fetchRecordsByParam = async (resource,param,idCondominio) => {
 
     const authToken = CookieService.get('access_token');
 
+    const headers = {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${authToken}`,
+    };
+
+    if(idCondominio) {
+        headers.idCondominio = idCondominio;
+    }
+
+    const options = {
+        url:`${api_url}${resource}/${param}`,
+        method: 'GET',
+        headers: headers,
+    };
+
     try {
-        const response =  await axios({
-            url:`${api_url}${resource}/${param}`,
-            method: 'GET',
-            headers: {
-                "Content-Type": "application/json",
-                "Authorization": 'Bearer ' + authToken,
-            },
-        });
+        const response = await axios(options);
+
+        if(response) {
+            return response.data;
+        }
 
         if(response) {
             return response.data;
